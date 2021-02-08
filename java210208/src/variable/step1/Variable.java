4dbeb378e00d6fbe4c141e78d39895e462daa495
kanggiwu <wu10604@gmail.com>
2021-02-08 17:32:29 +0900

    Variable 클래스 추가

---
+++ java210208/src/variable/step1/Variable.java
@@ -0,0 +1,24 @@
+package variable.step1;
+
+public class Variable {
+	//선언부
+	
+	//메소드 선언
+	
+	//메인 메소드: exe파일로 만들 수 있다.
+	/*
+	 * @return void ..돌려 받을 수 있는 값이 없다. 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {	
+		//System: 내 pc, out: 출력장치(모니터,프린터), print(출력)
+		//print메소드 여러개 = 같은 이름의 메소드 여러개 가능
+		//SELECT 이름 FROM 회원 집합 WHERE 아이디 = 'test' AND 비번 = '123'
+		
+		System.out.println();//ln 줄바꿈
+		System.out.print(5);//줄바꿈X
+		
+	}
+
+}
