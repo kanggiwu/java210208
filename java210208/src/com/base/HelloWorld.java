50987cc42dad7bcfed7d6f891e1c80cdcec0d32f
kanggiwu <wu10604@gmail.com>
2021-02-08 17:33:48 +0900

    HelloWorld 수정

--- java210208/src/com/base/HelloWorld.java
+++ java210208/src/com/base/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		HelloWorld hw = new HelloWorld();
 		System.out.println("당신의 나이는: " +hw.age+"입니다.");
+		System.out.println("당신의 나이는: " +hw.age+"입니다.");
 	}
 
 }
