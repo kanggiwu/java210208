6503c0e9832cd1d2893c1549b27191c02081e2c4
kanggiwu <wu10604@gmail.com>
2021-02-10 17:43:26 +0900

    반복문 연습

---
+++ java210208/src/fortest/Fortest.java
@@ -0,0 +1,43 @@
+package fortest;
+
+public class Fortest {
+
+	public static void main(String[] args) {
+		
+		/*
+		int sum = 0;
+		for(int i=1; i<=10;i++) {
+			if(i%2==0) {
+				sum=sum+i;
+			}
+		}
+		System.out.println("sum===>"+sum);
+	
+		int j=1;
+		int sum2 = 0;
+		while(j<=10) {
+			
+			
+			if(j%2==0) {
+				sum2 = sum2 + j;
+			}
+			j++;
+		}
+		System.out.println("sum2===>"+sum2);
+		*/
+		int i;
+		int evenSum = 0;
+		int oddSum = 0;
+		for(i = 1; i <=10; i = i + 1) {
+			if(i%2==0) {
+				evenSum +=i;
+			}
+			else {
+				oddSum +=i;
+			}
+		}System.out.println("짝수의 합:" +evenSum + ", 홀수의 합: "+oddSum);
+	
+	}
+	
+		
+}
